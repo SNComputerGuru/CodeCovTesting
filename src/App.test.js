d8cf1e59425253690f3a5ccc9c26d757930cba29
SNComputerGuru <shillingndivho4@gmail.com>
2025-05-03 17:12:34 +0200

    Initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
+// src/App.test.js
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Hi there text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getByText(/Hi there/i);
+  expect(element).toBeInTheDocument();
 });
